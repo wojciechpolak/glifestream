eab0adc6e306d15a13ee2b51b1c49ba361a412ad
Wojciech Polak <polak@gnu.org>
2010-12-30 18:22:34 +0100

    Add TinyMCE editor.

--- static/js/glifestream.js
+++ static/js/glifestream.js
@@ -388,18 +388,24 @@
     var docs = $('input[name=docs]');
     if (docs.length && docs.get (0).files && docs.get (0).files.length)
       return true;
-    var status = $('#status');
-    if ($.trim (status.val ()) != '') {
+    if (window.tinyMCE)
+      var content = tinyMCE.get ('status').getContent ();
+    else
+      var content = $('#status').val ();
+    if ($.trim (content) != '') {
       show_spinner (this);
       $.post (baseurl + 'api/share', { sid: $('#status-class').val (),
-	    content: status.val (),
+	    content: content,
 	    draft: $('#draft').attr ('checked') ? 1 : 0,
 	    friends_only: $('#friends-only').attr ('checked') ? 1 : 0 },
 	function (html) {
 	  hide_spinner ();
 	  $('#stream').prepend (html);
 	  $('#stream article:first a.map').each (render_map);
-	  status.val ('');
+	  if (window.tinyMCE)
+	    tinyMCE.get ('status').setContent ('');
+	  else
+	    $('#status').val ('');
 	  $('#share .fieldset').slideUp ();
 	  scaledown_images ('#stream article:first img');
 	});
@@ -750,6 +756,31 @@
       if (typeof window.continuous_reading != 'undefined')
 	continuous_reading = parseInt (window.continuous_reading, 10);
       nav_next.click (continuous_reading ? load_entries : follow_href);
+
+      if (window.tinyMCE) {
+	tinyMCE.init ({
+	  mode: 'exact',
+	  elements: 'status',
+	  width: '100%',
+	  convert_urls: false,
+	  extended_valid_elements : 'div[*]',
+	  plugins: 'insertdatetime,preview',
+	  theme: 'advanced',
+	  theme_advanced_toolbar_location: 'top',
+	  theme_advanced_toolbar_align: 'left',
+	  theme_advanced_statusbar_location: 'bottom',
+	  theme_advanced_resizing: true,
+	  theme_advanced_buttons1: 'bold,italic,underline,strikethrough,' +
+	      '|,justifyleft,justifycenter,justifyright,justifyfull,' +
+	      '|,formatselect,fontselect,fontsizeselect',
+	  theme_advanced_buttons2: 'cut,copy,paste,pastetext,pasteword,' +
+	      '|,search,replace,|,bullist,numlist,' +
+	      '|,outdent,indent,blockquote,|,undo,redo,' +
+	      '|,link,unlink,anchor,image,cleanup,code,' +
+	      '|,insertdate,inserttime,preview,|,forecolor,backcolor',
+	  theme_advanced_buttons3: ''
+        });
+      }
 
       if (window.audio_embeds)
 	$.extend (audio_embeds, window.audio_embeds);
