58525ab9173256e32159c73ec93aa2e5b2fcbd6f
Wojciech Polak <polak@gnu.org>
2012-10-15 20:02:34 +0200

    Update Twitter API to v1.1

--- apis/twitter.py
+++ apis/twitter.py
@@ -1,4 +1,4 @@
-#  gLifestream Copyright (C) 2009, 2010 Wojciech Polak
+#  gLifestream Copyright (C) 2009, 2010, 2012 Wojciech Polak
 #
 #  This program is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License as published by the
@@ -13,28 +13,111 @@
 #  You should have received a copy of the GNU General Public License along
 #  with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
+from django.utils.html import strip_tags, strip_entities
 from glifestream.filters import expand, truncate, twyntax
+from glifestream.utils import httpclient
+from glifestream.utils.time import mtime, now
+from glifestream.stream.models import Entry
 from glifestream.stream import media
-import webfeed
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
 OAUTH_REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 OAUTH_AUTHORIZE_URL     = 'https://api.twitter.com/oauth/authorize'
 OAUTH_ACCESS_TOKEN_URL  = 'https://api.twitter.com/oauth/access_token'
 
-class API (webfeed.API):
-    name = 'Twitter API'
+class API:
+    name = 'Twitter API v1.1'
     limit_sec = 120
 
+    def __init__ (self, service, verbose=0, force_overwrite=False):
+        self.service = service
+        self.verbose = verbose
+        self.force_overwrite = force_overwrite
+        if self.verbose:
+            print '%s: %s' % (self.name, self.service)
+
     def get_urls (self):
-        if not self.service.url and self.service.creds:
-            return ('http://api.twitter.com/1/statuses/home_timeline.atom?count=50',)
+        if not self.service.creds:
+            return ()
+        if not self.service.url:
+            return ('/1.1/statuses/home_timeline.json?count=50',)
         else:
             if not self.service.last_checked:
-                return ('http://api.twitter.com/1/statuses/user_timeline/%s.atom?count=200' %
+                return ('/1.1/statuses/user_timeline.json?screen_name=%s&count=200' %
                         self.service.url,)
             else:
-                return ('http://api.twitter.com/1/statuses/user_timeline/%s.atom' %
+                return ('/1.1/statuses/user_timeline.json?screen_name=%s' %
                         self.service.url,)
+
+    def run (self):
+        for url in self.get_urls ():
+            try:
+                self.fetch (url)
+            except:
+                pass
+
+    def fetch (self, url):
+        try:
+            hs = httpclient.gen_auth_hs (self.service,
+                                         'https://api.twitter.com' + url)
+            r = httpclient.get ('api.twitter.com', url, headers=hs, https=True)
+            if r.status == 200:
+                self.json = json.loads (r.data.decode ('utf_8'))
+                self.service.last_checked = now ()
+                self.service.save ()
+                self.process ()
+            elif self.verbose:
+                print '%s (%d) HTTP: %s' % (self.service.api,
+                                            self.service.id, r.reason)
+        except Exception, e:
+            if self.verbose:
+                import sys, traceback
+                print '%s (%d) Exception: %s' % (self.service.api,
+                                                 self.service.id, e)
+                traceback.print_exc (file=sys.stdout)
+
+    def process (self):
+        for ent in self.json:
+            guid = 'tag:twitter.com,2007:http://twitter.com/%s/status/%s' % \
+                (ent['user']['screen_name'], ent['id'])
+            if self.verbose:
+                print "ID: %s" % guid
+
+            t = datetime.datetime.strptime (ent['created_at'],
+                                            '%a %b %d %H:%M:%S +0000 %Y')
+            try:
+                e = Entry.objects.get (service=self.service, guid=guid)
+                if not self.force_overwrite and \
+                   e.date_updated and mtime (t.timetuple ()) <= e.date_updated:
+                    continue
+                if e.protected:
+                    continue
+            except Entry.DoesNotExist:
+                e = Entry (service=self.service, guid=guid)
+
+            e.guid = guid
+            e.title = 'Tweet: %s' % truncate.smart (
+                strip_entities (strip_tags (ent['text'])), max_length=40)
+            e.link  = 'https://twitter.com/%s/status/%s' % \
+                (ent['user']['screen_name'], ent['id'])
+            image_url = ent['user']['profile_image_url_https']
+            e.link_image = media.save_image (image_url, direct_image=False)
+
+            e.date_published = t
+            e.date_updated = t
+            e.author_name = ent['user']['name']
+            e.content = 'Tweet: %s' % expand.all (ent['text'])
+
+            try:
+                e.save ()
+                media.extract_and_register (e)
+            except:
+                pass
 
     def custom_process (self, e, ent):
         e.title = 'Tweet: %s' % truncate.smart (ent.title.split (': ', 1)[1])
