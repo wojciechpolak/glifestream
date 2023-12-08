e0c250d4d4d1229b7817c9a4b0da105ccdc4743c
Wojciech Polak <wojciechpolak@gmail.com>
2023-12-08 16:50:40 +0100

    Fixtures bugfix

--- conf/docker/entrypoint.sh
+++ conf/docker/entrypoint.sh
@@ -3,7 +3,15 @@
 export DJANGO_SETTINGS_MODULE=run.settings_docker
 
 python manage.py migrate --run-syncdb
-python manage.py loaddata glifestream/stream/fixtures/initial_data.json
+
+python manage.py shell <<EOL
+from glifestream.stream.models import Service
+if not Service.objects.exists():
+    print('Loading initial data...')
+    from django.core.management import call_command
+    call_command('loaddata', 'glifestream/stream/fixtures/initial_data.json')
+EOL
+
 python manage.py collectstatic --no-input
 python worker.py --init-files-dirs
 chgrp -R users /app/static/ && chmod -R g+w /app/static
